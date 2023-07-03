fcb071820beb5356653ab0ff5ae8f3280f9d387e
JuanManuelGlez <A00572003@tec.mx>
2023-07-03 10:45:41 -0600

    solución de la tarea de programación 2

--- Programa1.rkt
+++ Programa1.rkt
@@ -1,7 +1,19 @@
-#lang raquet
+#lang racket
 
-(define sum
-  (lambda (a b)
-    (a + b)))
+;;fahrenheit-to-celsius: Num =>Num
+(define fahrenheit-to-celsius
+  (lambda (f)
+    (/(*(- f 32)5)9)))
 
-(sum 10 4)
+(fahrenheit-to-celsius 212)
+(fahrenheit-to-celsius 32)
+(fahrenheit-to-celsius -40)
+
+;;roots: num,num,num=>num
+(define roots
+  (lambda (a b c)
+    (/(+(* -1 b)(sqrt(-(* b b)(* 4 a c))))(* 2 a))))
+
+(roots 2 4 2)
+(roots 1 0 0)
+(roots 4 5 1)
