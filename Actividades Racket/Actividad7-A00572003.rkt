6ddc1d35d153fdd83cb1604ab3f178abbfa698e6
JuanManuelGlez <A00572003@tec.mx>
2023-07-07 10:37:28 -0600

    Primeros tres problemas resueltos

--- Actividades Racket/Actividad7-A00572003.rkt
+++ Actividades Racket/Actividad7-A00572003.rkt
@@ -12,3 +12,15 @@
 (enlist '())
 (enlist '(a b c))
 (enlist '((1 2 3) 4 (5) 7 8))
+
+;;Invert-pairs: lst => lst.
+(define invert-pairs
+  (lambda (lst)
+    (cond
+      [(null? lst) '()]
+      [else (cons
+             (list (car( cdr (car lst))) (car (car lst)))
+            (invert-pairs (cdr lst)))]
+      )))
+
+(invert-pairs '((a 1)(a 2)(b 1)(b 2)))
